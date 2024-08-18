026fbb2fb66b29fb4809f42b826b4867f62442a6
Brian Kohan <bckohan@gmail.com>
2024-08-17 23:42:18 -0700

    incr version

--- sphinxcontrib/typer/__init__.py
+++ sphinxcontrib/typer/__init__.py
@@ -50,7 +50,7 @@ from typer.main import Typer, TyperGroup, TyperInfo
 from typer.main import get_command as get_typer_command
 from typer.models import Context as TyperContext
 
-VERSION = (0, 3, 4)
+VERSION = (0, 3, 5)
 
 __title__ = "SphinxContrib Typer"
 __version__ = ".".join(str(i) for i in VERSION)

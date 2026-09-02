25178a4034bb70c0826706a54d7545cadf6432b1
Brian Kohan <bckohan@gmail.com>
2026-09-02 16:37:28 -0700

    bump version, update changelog and drop python 3.10 support

--- src/sphinxcontrib/typer/__init__.py
+++ src/sphinxcontrib/typer/__init__.py
@@ -57,7 +57,7 @@ from typer.main import get_command as get_typer_command
 from typer.models import Context as TyperContext
 from typer.models import TyperInfo
 
-VERSION = (0, 9, 2)
+VERSION = (0, 10, 0)
 
 __title__ = "SphinxContrib Typer"
 __version__ = ".".join(str(i) for i in VERSION)
